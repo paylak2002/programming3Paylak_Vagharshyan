9c8703dba1c964f3e2be9b8b318b6b4ebc5e134d
Your Name <you@example.com>
2018-05-13 14:15:47 +0400

    prockt

--- app.js
+++ app.js
@@ -92,15 +92,15 @@ setInterval(function(){
 //////////////////////////////////////////////////////////////
 },500);
 
-// var express = require("express");
-// var app = express();
+var express = require("express");
+var app = express();
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
-// app.get("/", function(req, res){
-//    res.redirect("public/index.html");
-// });
+app.get("/", function(req, res){
+   res.redirect("public/index.html");
+});
 
-// app.listen(3000, function(){
-//    console.log("Example is running on port 3000");
-// });
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
