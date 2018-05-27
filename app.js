042fb108af957cdacdd75a9321f721ffaf841c75
Your Name <you@example.com>
2018-05-27 10:50:29 +0400

    port

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
-var port = process.env.PORT || 3000;
-
+app.set('port',process.env.PORT || 3000);
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
    res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(port, function(){
+server.listen(app.get('port'), function(){
    console.log("Start");
 });
 var exanak_multiply = 0;
